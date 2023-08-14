f9b686ca3946d2698790351c1700b9170462ac66
mandos1995 <mandos19950620@gmail.com>
2023-08-14 15:32:07 +0900

    🎉 Counter 프로젝트 생성

---
+++ Counter/Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by 김민석 on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
