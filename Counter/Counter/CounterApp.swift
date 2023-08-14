071aea5525d887d11d2bb8c8d5eff231dbb8292d
mandos1995 <mandos19950620@gmail.com>
2023-08-14 16:11:28 +0900

    Feat: Integrating into the app

--- Counter/Counter/CounterApp.swift
+++ Counter/Counter/CounterApp.swift
@@ -11,13 +11,14 @@ import ComposableArchitecture
 
 @main
 struct CounterApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            CounterView(
-              store: Store(initialState: CounterFeature.State()) {
-                CounterFeature()
-              }
-            )
+            CounterView(store: CounterApp.store)
         }
     }
 }
