e3f75ead6daf2d933b28c4b489e97ee8aea256e6
mandos1995 <mandos19950620@gmail.com>
2023-08-18 10:05:59 +0900

    Feat: Testing network requests

--- Counter/CounterFeatureTests/CounterFeatureTests.swift
+++ Counter/CounterFeatureTests/CounterFeatureTests.swift
@@ -47,5 +47,19 @@ final class CounterFeatureTests: XCTestCase {
         }
     }
     
+    func testNumberFact() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        await store.send(.factButtonTapped) {
+            $0.isLoading = true
+        }
+        
+        await store.receive(.factResponse("???"), timeout: .seconds(1)) {
+            $0.isLoading = false
+            $0.fact = "???"
+        }
+    }
+    
     
 }
