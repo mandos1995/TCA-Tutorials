94c72c6e1d5eba8d177698832acb482f7130bca8
mandos1995 <mandos19950620@gmail.com>
2023-08-18 11:45:00 +0900

    🎉 Contacts 프로젝트 생성

---
+++ Contacts/Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by 김민석 on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contacts/Contacts/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Contacts
+//
+//  Created by 김민석 on 2023/08/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
