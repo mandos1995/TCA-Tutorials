8cab89d29b0dffc57b5714b8184a8f808cead6a6
mandos1995 <mandos19950620@gmail.com>
2023-12-20 13:58:53 +0900

    feat: 네트워크 통신 기능 구현

--- TCA_Tutorial/Counter/CounterFeature.swift
+++ TCA_Tutorial/Counter/CounterFeature.swift
@@ -21,6 +21,7 @@ struct CounterFeature {
         case decrementButtonTapped
         case incrementButtonTapped
         case factButtonTapped
+        case factResponse(String)
     }
     
     var body: some ReducerOf<Self> {
@@ -40,6 +41,15 @@ struct CounterFeature {
                 state.fact = nil
                 state.isLoading = true
                 
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+            case .factResponse(let fact):
+                state.fact = fact
+                state.isLoading = false
                 return .none
             }
             
