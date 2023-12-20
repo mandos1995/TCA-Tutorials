c8dfb17153fc2649cdf8fae58263cbc9b09ed32b
mandos1995 <mandos19950620@gmail.com>
2023-12-20 10:06:21 +0900

    chore: 프로젝트 생성

---
+++ TCA_Tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA_Tutorial
+//
+//  Created by 김민석 on 12/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA_Tutorial/TCA_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TutorialApp.swift
+//  TCA_Tutorial
+//
+//  Created by 김민석 on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct TCA_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
