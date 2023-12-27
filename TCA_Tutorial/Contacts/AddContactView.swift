cd494a7063a21ff7863ffeac498b9bf87fc76200
mandos1995 <mandos19950620@gmail.com>
2023-12-27 14:51:34 +0900

    feat: Reducer 통합 구현

--- TCA_Tutorial/Contacts/AddContactView.swift
+++ TCA_Tutorial/Contacts/AddContactView.swift
@@ -31,20 +31,3 @@ struct AddContactView: View {
         }
     }
 }
-
-#Preview {
-    NavigationStack {
-          AddContactView(
-            store: Store(
-              initialState: AddContactFeature.State(
-                contact: Contact(
-                  id: UUID(),
-                  name: "Blob"
-                )
-              )
-            ) {
-              AddContactFeature()
-            }
-          )
-        }
-}
